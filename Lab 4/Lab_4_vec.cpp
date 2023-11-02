9fe7c06da4a18f4002e68971c904858e465b62db
miahz123 <64549330+miahz123@users.noreply.github.com>
2023-11-01 22:58:31 -0700

    Add files via upload

---
+++ Lab 4/Lab_4_vec.cpp
@@ -0,0 +1,33 @@
+/***
+ * CSE 2010 Fall 2023
+ * Lab #4
+ * Jeremiah Rivera 007634309
+ * 11/1/2023
+ *
+ * This program will organize data using data structures, arrays and vectors, and at the same time learning similarities and differences between the two in how they contain, access, and update elements, passing them onto functions.
+ *
+ * {Describe 1) what was most challenging and 2) how you solved/overcame the problem OR
+ * the directions of how you PLAN to solve it}
+ *
+***/
+
+#include <iostream>
+#include "ArrayVec.h"
+#include "DisplayVec.h"
+using namespace std;
+
+
+
+int main()
+{
+    // initialize vec
+
+    //initialize ArrayVec and DisplayVec
+
+
+    // populate with values
+
+    // display contents
+
+}
+
