131282b32a2672da6c288223fa7df1441676bbca
miahz123 <64549330+miahz123@users.noreply.github.com>
2023-11-06 23:56:05 -0800

    Add files via upload

--- Lab 4/DisplayVec.cpp
+++ Lab 4/DisplayVec.cpp
@@ -1,1 +1,43 @@
 //DisplayVec.cpp
+#include "DisplayVec.h"
+/**
+ * @brief Construct a new display vec
+ * 
+ */
+DisplayVec::DisplayVec(){
+
+
+}
+        void printVector(const vector<int> v){
+        for(int i = 0; i < v.size(); i++){
+            cout << v[i] << endl;
+        }
+
+        }
+        int DisplayVec::getVectMemSize(const vector<int> v){
+
+            int mem_size = 0;
+            for(int i = 0; i < v.size(); i++){
+                mem_size += sizeof(v[i]);
+            }
+            return mem_size
+
+        }
+        const int * DisplayVec::getAddress(vector<int> v, int index){
+        int * addr = &v[index];
+        return addr;
+    
+        }
+/**
+ * @brief prints total size of v and the memory locations of elements in v
+ * 
+ * @param v 
+ */
+    int DisplayVec::printMetaVector(const vector<int> v){
+    int mem_size = getVectMemSize(v);
+    int int_size = sizeof(v[0]);
+    printf("Array has %i number of integers. Each integer is worth %i bytes, totaling up to %i bytes of memory.\n", v.size(), int_size, mem_size);
+    printf("Element at index %i is located at (%p)\n", 0, getAddress(v, 0));
+    printf("Element at index %i is located at (%p)\n", 1, getAddress(v, 1));
+    printf("Element at index %i is located at (%p)\n", v.size()-1 ,getAddress(v, v.size()-1));
+    }
--- Lab 4/Lab_4_vec.cpp
+++ Lab 4/Lab_4_vec.cpp
@@ -21,13 +21,23 @@ using namespace std;
 int main()
 {
     // initialize vec
+    const int CAPACITY = 10;
+    vector <int> test_vec(CAPACITY);
 
     //initialize ArrayVec and DisplayVec
-
+    DisplayVec disp = DisplayVec();
+    ArrayVec av = ArrayVec();
 
     // populate with values
+    int range = 20;
+    int num_elem = 15;
+    av.randGenVec(test_vec, num_elem, range);
 
     // display contents
-
+    int test_vector_size = test_vec.size();
+    cout << "Vector Elements\n";
+    disp.printVector(test_vec);
+    cout << "Array Memsize";
+    disp.printMetaVector(test_vec);
 }
 
