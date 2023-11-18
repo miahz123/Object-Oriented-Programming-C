6d60c12fd4db081c41b7ae0728ccfa0977b7d4c7
miahz123 <64549330+miahz123@users.noreply.github.com>
2023-11-17 22:35:14 -0800

    Add files via upload

--- Lab 5/Lab_5.cpp
+++ Lab 5/Lab_5.cpp
@@ -6,8 +6,7 @@
  *
  * This lab iterates through arrays and/or vectors to search for elements, while monitoring the computation time and graphing our findings.
  *
- * {Describe 1) what was most challenging and 2) how you solved/overcame the problem OR
- * the directions of how you PLAN to solve it}
+ * The most difficult part of this lab was creating sa with arr_n elements. I overcame this by making sure the code understood that the integer n is the arr_n[i] elements.
  *
 ***/
 
@@ -65,19 +64,20 @@ int main()
                             500000, 1000000, 2000000, 5000000, 10000000, 100000000, 500000000};
 
     // create vector of runs for binarySearch
-
+    vector<double>bin_run;
 
     //create a for loop to go through all the element sizes
     for(int i = 0; i < BIN_NUM_SIZES; i++) {
         // create sa with vectors of up to arr_n[i] elements
-
+        int n = arr_n2[i];
+        SearchAnalysis sa2 = SearchAnalysis(n, exclusive_range);
 
         // record how long binarySearch runs for and append the time to your vector for binarySearch runs
-
+        double elapsed_bin_time = sa2.runTime(false, elem);
+        bin_run.push_back(elapsed_bin_time);
     }
     
     // save the times under BigO_binarySearch.csv
-
-
+    string filename_bin = "BigO_binarySearch.csv";
+    sa.writeToFile(filename_bin, bin_run, arr_n2);
 }
-
